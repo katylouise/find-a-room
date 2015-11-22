55fb008bcf43e1933dc596b53996258dce32ca8e
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-22 18:05:48 +0000

    Dynamic content loading correctly along with names of friends

--- js/app.js
+++ js/app.js
@@ -34,25 +34,28 @@ $(document).foundation();
 $(document).ready(function() {
   $('.room-link').click(function() {
     var room = $(this).attr('class').split(' ')[0];
-    $('.room-description').load("rooms/animal-friendly.html");
     $('.room-description').show();
-    displayFriends(room);
+    $('.room-description').load("rooms/animal-friendly.html", function() {
+      displayFriends(room);
+    });
   });
 
   function displayFriends(room) {
     var friendsToDisplay = friendsObj[room]["friends"];
+    //need to sort names alphabetically
+
     if (friendsToDisplay.length == 1) {
-      $('.friend-info').text(friendsToDisplay[0] + ' has stayed here');
+      $('.room-description .friend-info').text(friendsToDisplay[0] + ' has stayed here');
     }
     if (friendsToDisplay.length == 2) {
-      $('.friend-info').text(friendsToDisplay[0] + ' and ' + friendsToDisplay[1] + ' have stayed here');
+      $('.room-description .friend-info').text(friendsToDisplay[0] + ' and ' + friendsToDisplay[1] + ' have stayed here');
     }
     if (friendsToDisplay.length == 3) {
-      $('.friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and 1 other friend have stayed here');
+      $('.room-description .friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and 1 other friend have stayed here');
     }
     if (friendsToDisplay.length > 3) {
       var numFriendsLeft = friendsToDisplay.length - 2;
-      $('.friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and ' + numFriendsLeft + ' other friends have stayed here');
+      $('.room-description .friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and ' + numFriendsLeft + ' other friends have stayed here');
     }
   }
 });
