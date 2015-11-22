9bf78c8d203fe239e5f42c00dd783c6229979cce
Rebecca Appleyard <rklappleyard@gmail.com>
2015-11-22 16:47:01 +0000

    JS function written to display friends - needs refactoring

--- js/app.js
+++ js/app.js
@@ -1,11 +1,57 @@
+var friendsObj = {
+  "deluxe": {
+    "friends": [
+      "Bob Smith",
+      "Jane Doe",
+      "Bubba Hyde",
+      "Betsy Toheavens"
+    ]
+  },
+  "shared": {
+    "friends": [
+      "Bob Smith"
+    ]
+  },
+  "animal-friendly": {
+    "friends": [
+      "Bob Smith",
+      "Jane Doe",
+      "Bubba Hyde"
+    ]
+  },
+  "super-deluxe": {
+    "friends": [
+      "Bob Smith",
+      "Jane Doe"
+    ]
+  },
+  "another": {
+    "friends": []
+  }
+}
+
 $(document).foundation();
 $(document).ready(function() {
-  $('.animal-link').click(function() {
+  $('.room-link').click(function() {
     $('.room-description').show();
-    displayFriends();
+    var room = $(this).attr('class').split(' ')[0];
+    displayFriends(room);
   });
 
   function displayFriends(room) {
-
+    var friendsToDisplay = friendsObj[room]["friends"];
+    if (friendsToDisplay.length == 1) {
+      $('.friend-info').text(friendsToDisplay[0] + ' has stayed here');
+    }
+    if (friendsToDisplay.length == 2) {
+      $('.friend-info').text(friendsToDisplay[0] + ' and ' + friendsToDisplay[1] + ' have stayed here');
+    }
+    if (friendsToDisplay.length == 3) {
+      $('.friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and 1 other friend have stayed here');
+    }
+    if (friendsToDisplay.length > 3) {
+      var numFriendsLeft = friendsToDisplay.length - 2;
+      $('.friend-info').text(friendsToDisplay[0] + ', ' + friendsToDisplay[1] + ', and ' + numFriendsLeft + ' other friends have stayed here');
+    }
   }
 });
